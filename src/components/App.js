77c2b467fc20089b0e9a7b5371ff4820c341ebb0
Matt Eva <matteweva@gmail.com>
2021-11-09 15:50:49 -0800

    verbose sort functionality added

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,18 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [unsortedToys, setUnsortedToys] = useState([])
   const [toyData, setToyData] = useState([]);
-
+  
 
   useEffect(() =>{
     fetch(`http://localhost:3001/toys`)
       .then(r => r.json())
       .catch(error => console.error(error))
-      .then(data => setToyData(data))
+      .then(data => {
+        setToyData([...data])
+        setUnsortedToys([...data])
+      })
   }, [])
 
   function handleClick() {
@@ -32,14 +36,20 @@ function App() {
     fetch("http://localhost:3001/toys", configObj)
     .then(r => r.json())
     .catch(error => console.error(error))
-    .then(data => setToyData([...toyData, data]))
+    .then(data => {
+      setUnsortedToys([...unsortedToys, data])
+      setToyData([...toyData, data])
+    })
   }
 
   const deleteToy = (id) =>{
     fetch(` http://localhost:3001/toys/${id}`, {method: "DELETE"})
       .then(() =>{
         const oneLess = toyData.filter(toy => toy.id !== id)
+        const unorderedOneLess = unsortedToys.filter(toy => toy.id !== id)
+
         setToyData([...oneLess])
+        setUnsortedToys([...unorderedOneLess])
       })
   }
 
@@ -64,9 +74,29 @@ function App() {
           return item
         }
       })
+      const unsortedMoreLike = unsortedToys.map(item =>{
+        if (item.id === toy.id){
+          return upToy
+        } else{
+          return item
+        }
+      })
       setToyData(oneMoreLike)
+      setUnsortedToys(unsortedMoreLike)
     })
+  }
 
+  const sortToys = (value)=>{
+    if (value === "no sort"){
+      console.log(value)
+      console.log(unsortedToys)
+      setToyData([...unsortedToys])
+    } else if(value === "likes"){
+      const sortedLikes = toyData.sort((a,b) =>{
+        return b.likes - a.likes;
+      })
+      setToyData([...sortedLikes])
+    }
   }
 
   if(toyData.length === 0){
@@ -80,7 +110,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer toyData={toyData} deleteToy={deleteToy} addLike={addLike}/>
+      <ToyContainer toyData={toyData} deleteToy={deleteToy} addLike={addLike} sortToys={sortToys}/>
     </>
   );
 }
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer({toyData, deleteToy, addLike}) {
+function ToyContainer({toyData, deleteToy, addLike, sortToys}) {
   const toyCollection = toyData.map(toy => <ToyCard toy={toy} deleteToy={deleteToy} addLike={addLike} key={toy.id}/>)
+  const [selectedValue, setSelectedValue] = useState('')
+
   return (
-    <div id="toy-collection">{toyCollection}</div>
+    <>
+      <select value={selectedValue} onChange={(e)=>{
+        setSelectedValue(e.target.value)
+        sortToys(e.target.value)
+      }}> 
+        <option value="no sort">No Sort</option>
+        <option value="likes">Sort By Likes</option>
+      </select>
+      <div id="toy-collection">
+        {toyCollection}
+      </div>
+    </>
   );
 }
 
